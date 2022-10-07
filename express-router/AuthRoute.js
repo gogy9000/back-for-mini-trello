45e313735fffbbafad684582fe65646946308b1e
gogy9000 <goropashnyj9000@gmail.com>
2022-10-07 22:58:21 +0500

    create CRUD

--- express-router/AuthRoute.js
+++ express-router/AuthRoute.js
@@ -1,8 +1,6 @@
 
 const express = require('express');
-const bodyParser = require("body-parser");
 const authRouter = express.Router();
-const jsonParser = bodyParser.json()
 
 authRouter.get('/me',(req,res)=>{
     res.send({email: "string", id: "string", login: "string",resultCode:0})
